61bacc192ae7f71100509abd881359925fd51e27
mklee <55345539+MyeongkyuLee@users.noreply.github.com>
2021-06-08 18:42:56 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -51,6 +51,31 @@
     });
 
     // Create timeline
+    $('#Publication-timeline').each(function() {
+
+        $this = $(this); // Store reference to this
+        $userContent = $this.children('div'); // user content
+
+        // Create each timeline block
+        $userContent.each(function() {
+            $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
+        });
+
+        // Add icons to each block
+        $this.find('.vtimeline-point').each(function() {
+            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
+        });
+
+        // Add dates to the timeline if exists
+        $this.find('.vtimeline-content').each(function() {
+            var date = $(this).data('date');
+            if (date) { // Prepend if exists
+                $(this).parent().prepend('<span class="vtimeline-date">'+date+'</span>');
+            }
+        });
+
+    });
+    // Create timeline
     $('#experience-timeline').each(function() {
 
         $this = $(this); // Store reference to this
@@ -75,7 +100,6 @@
         });
 
     });
-
     // Open mobile menu
     $('#mobile-menu-open').click(function() {
         $('header, body').addClass('active');
